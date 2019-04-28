784b3bb680c1c17f2c2bfac4f21ff8e143d1fe12
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2019-04-27 19:44:43 -0700

    Working on graphQL access

--- src/main/java/org/cyclopsgroup/gitcon/github/GithubResourceRepository.java
+++ src/main/java/org/cyclopsgroup/gitcon/github/GithubResourceRepository.java
@@ -1,22 +1,21 @@
 package org.cyclopsgroup.gitcon.github;
 
+import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import javax.net.ssl.HttpsURLConnection;
+import org.apache.commons.io.IOUtils;
 import org.cyclopsgroup.gitcon.Resource;
 import org.cyclopsgroup.gitcon.ResourceRepository;
 
 /**
  * Example query:
- * 
- * query { 
- *   repository(owner: "jiaqi", name: "cloudpave-runtime-config") {
- *     content:object(expression: "master:cyclone-service/cyclone-service-local.properties") {
- *      ... on Blob {
- *         text
- *       }
- *     }
- *   }
- * }
+ *
+ * <p>query { repository(owner: "jiaqi", name: "cloudpave-runtime-config") {
+ * content:object(expression: "master:cyclone-service/cyclone-service-local.properties") { ... on
+ * Blob { text } } } }
  */
 public class GithubResourceRepository implements ResourceRepository {
   private class ResourceImpl extends Resource {
@@ -28,8 +27,7 @@ public class GithubResourceRepository implements ResourceRepository {
 
     @Override
     public InputStream openToRead() throws IOException {
-      // TODO Auto-generated method stub
-      return null;
+      return post(blobPath);
     }
 
     @Override
@@ -39,17 +37,15 @@ public class GithubResourceRepository implements ResourceRepository {
     }
   }
 
-  private final String accessToken;
-  private final String branchName;
+  private String accessToken = "";
+  private String branchName = "master";
+
   private final String githubUser;
   private final String repositoryName;
 
-  public GithubResourceRepository(String accessToken, String githubUser, String repositoryName,
-      String branchName) {
-    this.accessToken = accessToken;
+  public GithubResourceRepository(String githubUser, String repositoryName) {
     this.githubUser = githubUser;
     this.repositoryName = repositoryName;
-    this.branchName = branchName;
   }
 
   @Override
@@ -58,5 +54,53 @@ public class GithubResourceRepository implements ResourceRepository {
       filePath = filePath.substring(1);
     }
     return new ResourceImpl(filePath);
+  }
+
+  /** @param accessToken the accessToken to set */
+  public void setAccessToken(String accessToken) {
+    this.accessToken = accessToken;
+  }
+
+  /** @param branchName the branchName to set */
+  public void setBranchName(String branchName) {
+    this.branchName = branchName;
+  }
+
+  private InputStream post(String filePath) throws IOException {
+    URL target = new URL("https://api.github.com/graphql");
+    HttpsURLConnection con = (HttpsURLConnection) target.openConnection();
+
+    con.setRequestMethod("POST");
+    if (!accessToken.isEmpty()) {
+      con.setRequestProperty("Authorization", "bearer " + accessToken);
+    }
+    con.setDoOutput(true);
+
+    String bodyFormat =
+        IOUtils.toString(getClass().getResource("get_blob_content.gql"), StandardCharsets.UTF_8);
+    String data = String.format(bodyFormat, githubUser, repositoryName, branchName, filePath);
+
+    con.getOutputStream().write(data.getBytes(StandardCharsets.UTF_8));
+    con.getOutputStream().flush();
+    con.getOutputStream().close();
+
+    InputStream in =
+        new FilterInputStream(con.getInputStream()) {
+          @Override
+          public void close() throws IOException {
+            try {
+              super.close();
+            } finally {
+              con.disconnect();
+            }
+          }
+        };
+    return in;
+  }
+
+  public static void main(String[] args) throws IOException {
+    GithubResourceRepository repo = new GithubResourceRepository("jiaqi", "jcli");
+    String s = IOUtils.toString(repo.getResource("pom.xml").openToRead(), StandardCharsets.UTF_8);
+    System.out.println(s);
   }
 }
