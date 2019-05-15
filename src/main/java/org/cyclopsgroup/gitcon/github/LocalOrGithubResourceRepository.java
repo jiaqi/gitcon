84fab75612c35658bffe53bc0519930ed2d8cc5e
Jiaqi Guo <jiaqi.guo@gmail.com>
2019-05-15 07:43:22 -0700

    Finish local/github repo

--- src/main/java/org/cyclopsgroup/gitcon/github/LocalOrGithubResourceRepository.java
+++ src/main/java/org/cyclopsgroup/gitcon/github/LocalOrGithubResourceRepository.java
@@ -1,26 +1,37 @@
 package org.cyclopsgroup.gitcon.github;
 
 import java.io.File;
+import java.io.IOException;
 import org.cyclopsgroup.gitcon.FileSystemResourceRepository;
 import org.cyclopsgroup.gitcon.Resource;
 import org.cyclopsgroup.gitcon.ResourceRepository;
 
 public class LocalOrGithubResourceRepository implements ResourceRepository {
+  private static ResourceRepository createFileSystemRepository() {
+    return new FileSystemResourceRepository(new File(getPropertyOrFail("gitcon.dir")));
+  }
+
+  private static ResourceRepository createGithubRepository(String user, String project)
+      throws IOException {
+    return new GithubResourceRepository(user, project, getPropertyOrFail("gitcon.token"));
+  }
+
+  private static String getPropertyOrFail(String name) {
+    String value = System.getProperty(name);
+    if (value == null) {
+      throw new IllegalArgumentException(
+          "System property " + name + " is required but not defined.");
+    }
+    return value;
+  }
+
   private final ResourceRepository delegate;
 
-  private static ResourceRepository createFileSystemRepository() {
-    String dir = System.getProperty("gitcon.dir", "src/main/config");
-    return new FileSystemResourceRepository(new File(dir));
-  }
-
-  private static ResourceRepository createGithubRepository() {
-    return null;
-  }
-
-  public LocalOrGithubResourceRepository() {
+  public LocalOrGithubResourceRepository(String githubUser, String githubProject)
+      throws IOException {
     String type = System.getProperty("gitcon.type", "file");
     if (type.equalsIgnoreCase("github")) {
-      this.delegate = createGithubRepository();
+      this.delegate = createGithubRepository(githubUser, githubProject);
     } else {
       this.delegate = createFileSystemRepository();
     }
